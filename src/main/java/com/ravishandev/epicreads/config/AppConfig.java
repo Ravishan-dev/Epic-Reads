27c18bb9874242b17c2daaf73563a48647df93c2
Ravishan Samarasinghe <ravishansamarasinghe31@gmail.com>
2026-06-01 11:08:55 +0530

    Start Tomcat Server and Configure REST

---
+++ src/main/java/com/ravishandev/epicreads/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.ravishandev.epicreads.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class AppConfig extends ResourceConfig {
+    public AppConfig(){
+        packages("com.ravishandev.epicreads.controller");
+    }
+}
