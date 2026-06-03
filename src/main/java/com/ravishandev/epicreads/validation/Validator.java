68738bd45810280556d4dda8457b923ffd494e11
Ravishan Samarasinghe <ravishansamarasinghe31@gmail.com>
2026-06-03 08:31:49 +0530

    Create Useful Utilities and Password Validation Regex

---
+++ src/main/java/com/ravishandev/epicreads/validation/Validator.java
@@ -0,0 +1,5 @@
+package com.ravishandev.epicreads.validation;
+
+public class Validator {
+    public static final String PASSWORD_VALIDATION = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+}
